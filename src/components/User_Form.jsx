975d0b0d1f1cc39b77425e71f053df414f5de915
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2023-07-25 15:31:57 +0100

    linking_pages

--- src/components/User_Form.jsx
+++ src/components/User_Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles/styles.css";
 import Button from "./Button";
@@ -10,11 +10,38 @@ import Finishing_up from "./Finishing_up";
 import Success from "./Success";
 
 const User_Form = () => {
-  // const navigate = useNavigate();
+  const [step, setStep] = useState(1);
+  const [formValues, setFormValues] = useState({});
 
-  // const handleEventClick = () => {
-  //   navigate("");
-  // };
+  const handleEventNext = (data) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      ...data,
+    }));
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleEventPrevious = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <Personal_info onNext={handleEventNext} />;
+      case 2:
+        return <Select_your_plan onNext={handleEventNext}  onPrevious={handleEventPrevious}/>;
+      case 3:
+        return <Pick_add_ons onNext={handleEventNext} onPrevious={handleEventPrevious}/>;
+      case 4:
+        return <Finishing_up onNext={handleEventNext} onPrevious={handleEventPrevious}/>;
+      case 5:
+        return <Success onNext={handleEventNext} onPrevious={handleEventPrevious}/>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="container">
       <div className="container-body">
@@ -23,19 +50,25 @@ const User_Form = () => {
         </div>
         <div className="form-body">
           <div className="form-input">
+
+            {renderStep()}
             {/* <Personal_info /> */}
             {/* <Select_your_plan/> */}
             {/* <Pick_add_ons/> */}
             {/* <Finishing_up/> */}
-            <Success/>
+            {/* <Success/> */}
           </div>
           <div className="form-footer">
             <div className="btn-container">
-              <Button className={"btnOutline"} title={"Go Back"} />
+              <Button
+                className={"btnOutline"}
+                title={"Go Back"}
+                btnEventHandler={handleEventPrevious}
+              />
               <Button
                 className={"btnBlue"}
                 title={"Next Step"}
-                // btnEventHandler={handleEventClick}
+                btnEventHandler={handleEventNext}
               />
             </div>
           </div>
